63d5657e640f33112786a817f42cd214b5f0e9f4
slaghuis <eslaghuis@gmail.com>
2021-08-19 10:17:25 +0200

    Update vl53l1x_node.cpp
    Correct Version

--- src/vl53l1x_node.cpp
+++ src/vl53l1x_node.cpp
@@ -2,14 +2,11 @@
 #include <functional>
 #include <memory>
 #include <string>
-#include <wiringPi.h> //Wiring Pi dependency
 
-#include "VL53L1X.h"
+#include "vl53l1x.h"  // Library for the range sensor
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
-
-static VL53L1X distanceSensor;
+#include "sensor_msgs/msg/range.hpp"
 
 using namespace std::chrono_literals;
 
@@ -22,39 +19,59 @@ class Vl53l1xPublisher : public rclcpp::Node
     Vl53l1xPublisher()
     : Node("VL53L1X_publisher"), count_(0)
     {
+      // Set a 500ms timeout on the sensor. (Stop waiting and respond with an error)
+      sensor.setTimeout(500);
 
-      // Setup the VL53L1X distance sensor
-      Wire.begin();
-      
-      // delay(200);
-      Serial.begin(115200);
-
-      Serial.println("VL53L1X Qwiic Test"); //Why this line??
-
-      if (distanceSensor.begin() == false) {
+      if (!sensor.init()) {
         RCLCPP_ERROR(this->get_logger(), "Sensor offline!");
       }
- 
+
+      // Use long distance mode and allow up to 50000 us (50 ms) for a measurement.
+      // You can change these settings to adjust the performance of the sensor, but
+      // the minimum timing budget is 20 ms for short distance mode and 33 ms for
+      // medium and long distance modes. See the VL53L1X datasheet for more
+      // information on range and timing limits.
+      sensor.setDistanceMode(Vl53l1x::Long);
+      sensor.setMeasurementTimingBudget(50000);
+
+      // Start continuous readings at a rate of one measurement every 50 ms (the
+      // inter-measurement period). This period should be at least as long as the
+      // timing budget.
+      sensor.startContinuous(100);
+
       // Setup the publisher
-      publisher_ = this->create_publisher<std_msgs::msg::String>("topic", 10);
+      publisher_ = this->create_publisher<sensor_msgs::msg::Range>("/vl53l1x/range", 5);
       timer_ = this->create_wall_timer(
       500ms, std::bind(&Vl53l1xPublisher::timer_callback, this));
     }
 
   private:
+    Vl53l1x sensor;
+
     void timer_callback()
     {
-      while (distanceSensor.newDataReady() == false)
-        delay(5);  //Correct this to a ROS2 compliant delay
-      int distance = distanceSensor.getDistance(); // Get the result of the measurement from the sensor
+      int distance = sensor.read_range();
+      if(sensor.timeoutOccurred()) {
+        RCLCPP_ERROR(this->get_logger(), "Timeout Occured!");
+        distance = 0;
+      }
 
-      auto message = std_msgs::msg::String();
-      message.data = "distance " + std::to_string(distance);
-      RCLCPP_INFO(this->get_logger(), "VL53L1X '%s'", message.data.c_str());
-      publisher_->publish(message);
+      auto message = sensor_msgs::msg::Range();
+      message.radiation_type = sensor_msgs::msg::Range::INFRARED;
+      message.field_of_view = 0.47;              // Typically 27 degrees or 0,471239 radians
+      message.min_range = 0.14;                  // 140 mm.  (It is actully much less, but this makes sense in the context
+      message.max_range = 3.00;                  // 3.6 m. in the dark, down to 73cm in bright light
+
+      message.range = (float) distance / 1000.0; // range in meters
+
+      // from https://github.com/ros2/common_interfaces/blob/master/sensor_msgs/msg/Range.msg
+      // # (Note: values < range_min or > range_max should be discarded)
+      if((message.range >= message.min_range) && (message.range <= message.max_range)) {
+        publisher_->publish(message);
+      }
     }
     rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<sensor_msgs::msg::Range>::SharedPtr publisher_;
     size_t count_;
 };
 
